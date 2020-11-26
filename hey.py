24f74ad6045a488854238e598f25c2c6efc4a290
manhduc07 <72090241+manhduc07@users.noreply.github.com>
2020-11-26 19:49:07 +0700

    Add files via upload

---
+++ hey.py
@@ -0,0 +1,10 @@
+list2 = [({'name': 'Peter', 'age':2}, {'name': 'John', 'age':21}), 
+         ({'name': 'Mary', 'age':2}, {'name': 'Trandanpro', 'age':21}), 
+         ({'name': 'Nam', 'age':2}, {'name': 'Hung', 'age':21}), 
+         ({'name': 'Mai', 'age':2}, {'name': 'Loan', 'age':21})]
+for n,x in enumerate(list2):
+    print(n,x)
+    for i in x:
+        print(i)
+        print('age',i['age'])
+        print('name',i ['name'])
